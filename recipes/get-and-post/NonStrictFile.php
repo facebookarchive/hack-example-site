11b2d111cb2b5eb4f317ce9e77875f53691565b0
Robert Lynch <BobertForever@users.noreply.github.com>
2015-09-23 12:52:26 -0500

    Update get-and-post recipe to use new Map initialization
    `Map::fromArray` seems to have been deprecated in favor of `new Map`. This updates this example to reflect that.

--- recipes/get-and-post/NonStrictFile.php
+++ recipes/get-and-post/NonStrictFile.php
@@ -13,12 +13,12 @@ function getGETParams(): Map<string, mixed> {
   // $_GET is not defined in code so Hack doesn't know about it and you can't
   // use it in strict mode. You can interact with it outside of strict mode,
   // though.
-  return Map::fromArray($_GET);
+  return new Map($_GET);
 }
 
 function getPOSTParams(): Map<string, mixed> {
   // Same deal with $_POST and other magically defined globals
-  return Map::fromArray($_POST);
+  return new Map($_POST);
 }
 
 // Same deal with $_SERVER
